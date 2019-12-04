510afe02229e79bf900f6a7acd8146b5a80907e4
AntenehAdmasu <antenehadmasu8@gmail.com>
2019-12-04 17:29:28 +0400

    modifications

--- DayEleven/RemoveDuplicates.java
+++ DayEleven/RemoveDuplicates.java
@@ -5,15 +5,11 @@ public class RemoveDuplicates {
         ListNode l2 = new ListNode(1);
         ListNode l3 = new ListNode(2);
         ListNode l4 = new ListNode(3);
-        ListNode l5 = new ListNode(3);
-//        ListNode l6 = new ListNode(4);
-//        ListNode l7 = new ListNode(5);
+        ListNode l5 = new ListNode(3); 
         l1.next = l2;
         l2.next = l3;
         l3.next = l4;
-        l4.next = l5;
-//        l5.next = l6;
-//        l6.next = l7;
+        l4.next = l5; 
 
         ListNode l = new RemoveDuplicates().deleteDuplicates(l1);
         while (l != null){
@@ -36,20 +32,14 @@ public class RemoveDuplicates {
         ListNode next = head.next;
         int current;
 
-        while(next != null){
-            System.out.println("In while");
-
-            System.out.println("head's val " + head.val);
-            System.out.println("next's val " + next.val);
+        while(next != null){ 
 
             if(head.val == next.val){
-
-                System.out.println("in if");
 
                 current = head.val;
                 next = next.next;
             }else{
-                System.out.println("in else");
+
                 head.next = next;
                 head = head.next;
                 next = head.next;
