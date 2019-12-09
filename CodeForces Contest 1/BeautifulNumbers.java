0c7ca35820dffd0495ef94f156d623437a2ac41b
AntenehAdmasu <antenehadmasu8@gmail.com>
2019-12-09 16:25:36 +0400

    Beautiful Numbers

---
+++ CodeForces Contest 1/BeautifulNumbers.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BeautifulNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numberOfInputs = scanner.nextInt();
+
+        ArrayList<String> userinputs = new ArrayList<>();
+
+
+        for (int i = 0; i  < numberOfInputs ; i++) {
+            scanner.nextInt();
+            scanner.nextLine();
+            String nums = scanner.nextLine();
+            userinputs.add(nums);
+        }
+
+        for (int i = 0; i < userinputs.size() ; i++) {
+            System.out.println(getBeautifulNumbers(userinputs.get(i)));
+        }
+        scanner.close();
+    }
+
+    public static String getBeautifulNumbers(String userinput){
+        String[] numbers = userinput.split(" ");
+        int size = numbers.length;
+
+        int[] arr = new int[size];
+        StringBuilder result = new StringBuilder(userinput);
+        for (int i = 0; i < size; i++) {
+            arr[Integer.parseInt(numbers[i]) - 1] = i;
+        }
+
+        int min = arr[0] + 1;
+        int max = arr[0] + 1;
+
+        for (int i = 0; i < size ; i++) {
+            min = min < (arr[i] + 1) ? min : arr[i] + 1;
+            max = max > (arr[i] + 1) ? max : arr[i] + 1;
+            char bit = ((max - min + 1) == (i+1) ) ? '1' : '0';
+            result.setCharAt(i,bit);
+        }
+        return result.substring(0,max);
+    }
+}
