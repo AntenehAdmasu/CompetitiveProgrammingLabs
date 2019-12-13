dccbf2070bdf7973d6f2672be705be3d9b50ffab
AntenehAdmasu <antenehadmasu8@gmail.com>
2019-12-13 07:31:50 +0400

    Maximum binary tree - modification

--- Day 14/MaximumBinaryTree.java
+++ Day 14/MaximumBinaryTree.java
@@ -3,22 +3,6 @@ import java.util.Arrays;
 public class MaximumBinaryTree {
 
     public static void main(String[] args) {
-        int[] x = new int[]{4,2,6,5,7,8};
-//        int[] y = Arrays.copyOfRange(x, 6, 6);
-
-//        System.out.println("len is " + y.length);
-//        for (int i = 0; i < y.length; i++) {
-//            System.out.println(y[i]);
-//        }
-
-//        int max = 0;
-//        for (int i = 0; i < x.length; i++) {
-//            if(x[i] > x[max]){
-//                max = i;
-//            }
-//        }
-//
-//        System.out.println("max is " + max);
         new MaximumBinaryTree().constructMaximumBinaryTree(x);
     }
 
@@ -37,21 +21,11 @@ public class MaximumBinaryTree {
             }
         }
 
-        System.out.println("max is " + max);
         TreeNode root = new TreeNode(nums[max]);
 
         leftarray = Arrays.copyOfRange(nums, 0, max);
         rightarray = Arrays.copyOfRange(nums, max+1, len);
 
-        System.out.println("left len " + leftarray.length);
-        System.out.println("right len " + rightarray.length);
-
-        for (int i = 0; i < leftarray.length; i++) {
-            System.out.println("L " + leftarray[i]);
-        }
-        for (int i = 0; i < rightarray.length; i++) {
-            System.out.println("R " + rightarray[i]);
-        }
 
         root.left = constructMaximumBinaryTree(leftarray);
         root.right = constructMaximumBinaryTree(rightarray);
