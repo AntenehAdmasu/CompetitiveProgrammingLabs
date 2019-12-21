b9eef0a555e38f8b07e56cc09fb3b887f54df4c2
AntenehAdmasu <antenehadmasu8@gmail.com>
2019-12-21 17:30:50 +0300

    Binary Search

---
+++ Day 19/BinarySearch.java
@@ -0,0 +1,40 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] A = new int[]{-1,0,3,5,9,12};
+        System.out.println(new BinarySearch().binarySearch(A,0,A.length -1,2));
+//        System.out.println(Math.log(100));
+
+    }
+
+    public int search(int[] nums,int middle,int target,int numofCalls){
+        numofCalls++;
+        if(nums[middle] == target) return middle;
+        double d = Math.ceil(((double)nums.length/(double)(2*numofCalls)));
+        if((int)d == 1) return -1;
+
+        if(nums[middle] < target){
+            middle = middle + (int)(d);
+            return search(nums, middle,target,numofCalls+1);
+
+        }else{
+            middle = middle - (int)(d);
+            return search(nums,middle,target,numofCalls+1);
+        }
+    }
+
+    public int binarySearch(int[] nums, int left, int right, int target){
+        System.out.println("left right " + left + " " + right);
+        if(right>= left){
+            int middle = (left + right - 1) / 2 ;
+            if(left == right) middle = left;
+
+            System.out.println("middle is " + middle);
+            if(nums[middle] == target) return middle;
+
+            if(nums[middle] > target) return binarySearch(nums, left, middle-1, target);
+            return binarySearch(nums, middle + 1, right,target);
+        }
+        return -1;
+
+    }
+}
