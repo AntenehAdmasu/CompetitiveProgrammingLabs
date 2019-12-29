64217b78c7167bc3d7c6f38ca7ba32e6c67c1b01
AntenehAdmasu <antenehadmasu8@gmail.com>
2019-12-29 21:47:10 +0300

    Optimization

--- Day 14/BottomLeftTreeValue.java
+++ Day 14/BottomLeftTreeValue.java
@@ -1,3 +1,5 @@
+// Question : https://leetcode.com/problems/find-bottom-left-tree-value/
+
 public class BottomLeftTreeValue {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
@@ -5,7 +7,6 @@ public class BottomLeftTreeValue {
         root.right = new TreeNode(3);
 
         root.left.left = new TreeNode(4);
-//        root.left = new TreeNode(2);
 
         System.out.println("Val is " + new BottomLeftTreeValue().findBottomLeftValue(root));
     }
@@ -17,31 +18,24 @@ public class BottomLeftTreeValue {
     }
 
     private int[] getBottomLeftValue(TreeNode root,int levelCounter) {
+
         levelCounter++;
-        System.out.println();
-        System.out.println("I am " + root.val);
+
         if(root.left == null & root.right == null){
-            System.out.println("Returning my self " + root.val + " " + (levelCounter));
-            return new int[]{levelCounter + 1, root.val};
+            return new int[]{levelCounter, root.val};
         }
 
         int[] leftResult = null;
         int[] rightResult = null;
 
-        if(root.left != null){
-            leftResult = getBottomLeftValue(root.left, levelCounter);
-        }
-        if(root.right != null){
-            rightResult = getBottomLeftValue(root.right, levelCounter);
-        }
-        if(leftResult == null){
-            System.out.println(root.val + " returning from right val: " + rightResult[1]);
-            return rightResult;
-        }
-        if(rightResult == null){
-            System.out.println(root.val + " returning from left val: " + leftResult[1]);
-            return leftResult;
-        }
+        if(root.left != null) leftResult = getBottomLeftValue(root.left, levelCounter);
+        
+        if(root.right != null) rightResult = getBottomLeftValue(root.right, levelCounter);
+
+        if(leftResult == null) return rightResult;
+        
+        if(rightResult == null) return leftResult;
+        
         if(leftResult[0] < rightResult[0]){
             return rightResult;
         }else{
