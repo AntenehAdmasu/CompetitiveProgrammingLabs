b5b8e2141760a0e42b9a5f7b90d1bcd5ccef3579
AntenehAdmasu <antenehadmasu8@gmail.com>
2019-12-30 22:40:15 +0300

    Reverse Node in k group

--- Day 11/ReverseNodesInKGroup.java
+++ Day 11/ReverseNodesInKGroup.java
@@ -1,43 +1,6 @@
 import java.util.List;
 
 public class ReverseNodesInKGroup {
-    public static void main(String[] args) {
-        ListNode a = new ListNode(1);
-        ListNode b = new ListNode(2);
-        ListNode c = new ListNode(3);
-        ListNode d = new ListNode(4);
-        ListNode e = new ListNode(5);
-        ListNode f = new ListNode(6);
-        ListNode g = new ListNode(7);
-        ListNode h = new ListNode(8);
-        ListNode i = new ListNode(9);
-        ListNode j = new ListNode(10);
-        ListNode m = new ListNode(11);
-        ListNode n = new ListNode(12);
-
-        a.next = b;
-        b.next = c;
-        c.next = d;
-        d.next = e;
-        e.next = f;
-        f.next = g;
-        g.next = h;
-        h.next = i;
-        i.next =  j;
-        j.next =  m;
-//        m.next =  n;
-
-
-        ListNode handle = new ReverseNodesInKGroup().reverseKGroup(a,15);
-
-        while (handle != null){
-//        for (int k = 0; k < 10; k++) {
-
-
-            System.out.println("#"+handle.val);
-            handle = handle.next;
-        }
-    }
 
 
     public ListNode reverseKGroup(ListNode head, int k) {
@@ -46,12 +9,10 @@ public class ReverseNodesInKGroup {
         ListNode pointer = new ListNode(0);
         ListNode start = null;
         ListNode endOfPrevious = null;
-
         ListNode next = null;
 
         int loopcount = 1;
         while (isEnough(head,k)){
-            System.out.println("head is " + head.val);
 
             for (int i = 0; i < k; i++) {
                 if(i == 0) start = head;
@@ -65,7 +26,6 @@ public class ReverseNodesInKGroup {
 
             }
             if(loopcount == 1){
-                System.out.println("handler is " + previous.val);
                 pointer.next = previous;
             }
             if(loopcount > 1) endOfPrevious.next = previous;
@@ -84,8 +44,8 @@ public class ReverseNodesInKGroup {
         }
 
         return pointer.next;
-
     }
+
 
     public boolean isEnough(ListNode item, int k){
         for (int i = 0; i < k; i++) {
