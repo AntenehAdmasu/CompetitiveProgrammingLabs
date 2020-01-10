3abcaddb118906c35e0ac69c2c2e8bf497aeafb7
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-01-10 21:11:02 +0300

    codeforces contest 6

---
+++ Codeforces Contest6/MezoPlayingZoma.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MezoPlayingZoma {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfCommands = scanner.nextInt();
+        scanner.nextLine();
+        scanner.nextLine();
+        System.out.println(numberOfCommands + 1);
+        scanner.close();
+
+        }
+}
