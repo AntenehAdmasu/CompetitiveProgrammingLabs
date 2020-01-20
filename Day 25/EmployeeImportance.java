5697419c9f5bfeff98c2a2d0e7c0357a8eea3a4a
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-01-20 22:06:53 +0300

    Employee Importance

---
+++ Day 25/EmployeeImportance.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class EmployeeImportance {
+
+
+    public int getImportance(List<Employee> employees, int id) {
+        // Put all importance in a hashmap
+        HashMap<Integer,Employee> allemployees = new HashMap<>();
+        for (Employee e: employees){
+            Employee employee = new Employee();
+            employee.id = e.id;
+            employee.subordinates = e.subordinates;
+            employee.importance = e.importance;
+            allemployees.put(e.id,employee);
+        }
+
+        return getEmployeeImportance(allemployees,id);
+
+
+
+    }
+
+    public int getEmployeeImportance(HashMap<Integer, Employee> allEmployees, int id){
+        Employee self = allEmployees.get(id);
+        int myImportance = self.importance;
+        int childrenImportance = 0;
+
+        for (int Id: self.subordinates) {
+            childrenImportance += getEmployeeImportance(allEmployees,Id);
+        }
+
+        return myImportance + childrenImportance;
+    }
+
+
+
+}
+
+
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
