1a5cffbb965fbd8a48ed62911f62c2eed43e65bd
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-01-21 20:23:23 +0300

    update on flood fill

--- Day 25/FloodFill.java
+++ Day 25/FloodFill.java
@@ -14,21 +14,22 @@ public class FloodFill {
 
     public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int color = image[sr][sc];
+        
         if(color == newColor) return image;
         image[sr][sc] = newColor;
-
+        
         //left
-        if (sr - 1 >= 0 && image[sr - 1][sc] == color) floodFill(image, sr - 1, sc, newColor);
-
+        if(sr - 1 >= 0 && image[sr-1][sc] == color) floodFill(image, sr-1,sc,newColor);
+        
         //right
-        if (sr + 1 < image.length && image[sr + 1][sc] == color) floodFill(image, sr + 1, sc, newColor);
-
+        if(sr + 1 < image.length && image[sr+1][sc] == color) floodFill(image, sr+1,sc,newColor);
+        
         //up
-        if (sc - 1 >= 0 && image[sr - 1][sc] == color) floodFill(image, sr, sc - 1, newColor);
-
+        if(sc - 1 >= 0 && image[sr][sc-1] == color) floodFill(image, sr,sc-1,newColor);
+        
         //down
-        if (sr + 1 >= image[0].length && image[sr - 1][sc] == color) floodFill(image, sr, sc + 1, newColor);
-
+        if(sc + 1 < image[0].length && image[sr][sc+1] == color) floodFill(image, sr,sc+1,newColor);
+        
         return image;
 
     }
