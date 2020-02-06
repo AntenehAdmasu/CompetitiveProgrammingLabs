7fe4f42b81a68af124740503ba4db3044899e328
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-02-07 00:22:58 +0300

    Minimum path triangle sum

---
+++ Code Camp/Day5/MinimumPathTriangle.java
@@ -0,0 +1,17 @@
+// Question link : https://leetcode.com/problems/triangle/
+
+import java.util.List;
+
+public class MinimumPathTriangle {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i++) {
+            for (int j = 0; j < triangle.get(i).size() ; j++) {
+                int temp = triangle.get(i).get(j);
+                triangle.get(i).remove(j);
+                triangle.get(i).add(Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1)) + temp);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
