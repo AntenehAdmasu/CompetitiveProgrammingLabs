b59af73d867ba5e5ec704a908767c4384b01c4ff
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-02-20 16:30:36 +0300

    Optimized versionO(n)

--- Code Camp/Day14/LongestTurbulentSubarray.java
+++ Code Camp/Day14/LongestTurbulentSubarray.java
@@ -2,59 +2,45 @@
 
 public class LongestTurbulentSubarray {
 
-    public static void main(String[] args) {
-        int A[] = {9, 9, 9, 9, 9, 9};
-        int B[] = {9,4,2,10,7,8,8,1,9};
-        int C[] = {2,2,3};
-        System.out.println("==>> " + new LongestTurbulentSubarray().maxTurbulenceSize(B));
-    }
-
     public int maxTurbulenceSize(int[] A) {
-        if(A.length < 2){
-            return A.length;
-        }
-        if(A[1] > A[0]){
-            return getTurbulentSub(A, 0, 0, 1);
-        }else{
-            return getTurbulentSub(A, 0, 1, 1);
-        }
-    }
 
-    public int getTurbulentSub(int[] A, int index, int option, int valSofar) {    // Option 1  - find increasing Option 2 - find decreasing Option3 - whatever works for you
-        int val = 0;
+        boolean increasing = true;
 
-        if (index + 1 >= A.length) {
-            return valSofar;
-        }
+        int maxCount = 1;
+        int count = 1;
+        for (int i = 0; i < A.length-1 ; i++) {
 
-        if (option == 0) {
-            if (A[index + 1] > A[index]) { // find increasing
-                val = getTurbulentSub(A, index + 1, 1, valSofar + 1);
-            } else {
-                if (A[index + 1] < A[index]) {
-                    val = Math.max(valSofar,getTurbulentSub(A, index + 1, 0,  2));
-                } else if (A[index + 1] > A[index]) {
-                    val = Math.max(valSofar,getTurbulentSub(A, index + 1, 1,  2));
-                }else{
-                    val = Math.max(valSofar,getTurbulentSub(A, index + 1, 1,  1));
-                }
-
+            if(A[i] == A[i+1]){
+                count = 1;
+                maxCount = Math.max(count,maxCount);
+                continue;
             }
-        } else if (option == 1) {     // find decreasing
-            if (A[index + 1] < A[index]) {
-                val = getTurbulentSub(A, index + 1, 0, valSofar + 1);
-            } else {
-                if (A[index + 1] < A[index]) {
-                    val = Math.max(valSofar, getTurbulentSub(A, index + 1, 0,  2));
-                } else if (A[index + 1] > A[index]) {
-                    val = Math.max(valSofar,getTurbulentSub(A, index + 1, 1,  2));
-                }else{
-                    val = Math.max(valSofar,getTurbulentSub(A, index + 1, 1,  1));
-                }
 
+            if(increasing){
+
+                if(A[i+1] < A[i]){
+                    count++;
+                    maxCount = Math.max(maxCount,count);
+                    increasing = !increasing;
+
+                }else if(A[i+1] > A[i]){
+
+                    count = 2;
+                    maxCount = Math.max(maxCount, count);
+                }
+            }else{
+                if(A[i+1] > A[i]){
+                    count++;
+                    maxCount = Math.max(maxCount,count);
+                    increasing = !increasing;
+                }else if(A[i+1] < A[i]){
+                    count = 2;
+                    maxCount = Math.max(maxCount, count);
+                }
             }
         }
 
-        return val;
+        return maxCount;
+
     }
 }
