86b71a8642106e14310625587c169910c54696ff
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-02-26 15:25:21 +0300

    Polished code according to review

--- Code Camp/Day5/ShortestCommonSupersequence.java
+++ Code Camp/Day5/ShortestCommonSupersequence.java
@@ -5,65 +5,75 @@ public class ShortestCommonSupersequence {
     public static void main(String[] args) {
         new ShortestCommonSupersequence().shortestCommonSupersequence("abac", "cab");
     }
+    
     public String shortestCommonSupersequence(String str1, String str2) {
 
-        int[][] arr = new int[str1.length() + 1][str2.length() + 1];
+        int[][] lettersMatrix = getLettersMatrix(str1,str2);
+
+        int str1MatrixIndex = str1.length();
+        int str2MatrixIndex = str2.length();
+
+        StringBuilder result = new StringBuilder();
+
+        // This loop tracks back characters from the bottom right corner of the letter matrix
+        //  to find the result string
+        while (str1MatrixIndex > 0 && str2MatrixIndex > 0){
+            int str1Index = str1MatrixIndex - 1;
+            int str2Index = str2MatrixIndex - 1;
+
+            if(str1.charAt(str1Index) == str2.charAt(str2Index)){
+                result.insert(0,str1.charAt(str1Index));
+                str1MatrixIndex--;
+                str2MatrixIndex--;
+            }else{
+                if(lettersMatrix[str1MatrixIndex][str2MatrixIndex-1] < lettersMatrix[str1MatrixIndex-1][str2MatrixIndex]){
+                    result.insert(0,str2.charAt(str2Index));
+                    str2MatrixIndex--;
+                }else{
+                    result.insert(0,str1.charAt(str1Index));
+                    str1MatrixIndex--;
+                }
+            }
+        }
+
+        StringBuilder remaining = new StringBuilder();
+        if(str2MatrixIndex > 0){
+            remaining.append(str2.substring(0,str2MatrixIndex));
+        }
+        if (str1MatrixIndex > 0){
+            remaining.append(str1.substring(0,str1MatrixIndex));
+        }
+
+        result.insert(0,remaining);
+        
+        return result.toString();
+
+    }
+
+    // Constructs a matrix that represents each character's relation with characters of the other string
+    private int[][] getLettersMatrix(String str1, String str2) {
+
+        int[][] lettersMatrix = new int[str1.length() + 1][str2.length() + 1];
 
         for (int i = 0; i <= str1.length(); i++) {
-            arr[i][0] = i;
+            lettersMatrix[i][0] = i;
         }
 
         for (int j = 0; j <= str2.length(); j++) {
-            arr[0][j] = j;
+            lettersMatrix[0][j] = j;
         }
 
         for (int i = 1; i <= str1.length(); i++) {
             for (int j = 1; j <= str2.length(); j++) {
                 if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    arr[i][j] = arr[i - 1][j - 1] + 1;
-
+                    lettersMatrix[i][j] = lettersMatrix[i - 1][j - 1] + 1;
                 } else {
-                    arr[i][j] = Math.min(arr[i - 1][j], arr[i][j - 1]) + 1;
+                    lettersMatrix[i][j] = Math.min(lettersMatrix[i - 1][j], lettersMatrix[i][j - 1]) + 1;
                 }
             }
         }
-
-        int currentI = str1.length();
-        int currentJ = str2.length();
-
-        String result = "";
-
-        while (currentI > 0 && currentJ > 0){
-            int currenti = currentI - 1;
-            int currentj = currentJ - 1;
-
-            if(str1.charAt(currenti) == str2.charAt(currentj)){
-                result = str1.charAt(currenti) + result;
-                currentI--;
-                currentJ--;
-            }else{
-                if(arr[currentI][currentJ-1] < arr[currentI-1][currentJ]){
-                    result = str2.charAt(currentj) + result;
-                    currentJ--;
-                }else{
-                    result = str1.charAt(currenti) + result;
-                    currentI--;
-                }
-            }
-        }
-        String remaining = "";
-        if(currentJ > 0){
-            remaining = str2.substring(0,currentJ);
-        }
-        if (currentI > 0){
-            remaining = str1.substring(0,currentI);
-
-        }
-
-        result = remaining + result;
-
-        return result;
-
+        
+        return lettersMatrix;
     }
 
 }
