440678d5584746cc5e0268918fe5e0feb82c024c
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-02-27 00:41:37 +0300

    polishing

--- Day 26/ValidParentheses.java
+++ Day 26/ValidParentheses.java
@@ -5,9 +5,11 @@ public class ValidParentheses {
     public int minAddToMakeValid(String S) {
         int openCounter = 0;
         int closedCounter = 0;
+        
         for(int i = 0; i < S.length(); i++){
-            if(S.charAt(i) == '(') openCounter++;
-            else{
+            if(S.charAt(i) == '('){
+                openCounter++;
+            }else{
                 if(openCounter > 0){
                     openCounter--;
                 }else{
