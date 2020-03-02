d64809222f60b96e7b5600da5810008d141c1399
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-03-03 00:45:28 +0300

    Code Restructuring

--- Day 25/BinaryTreeMaximumPathSum.java
+++ Day 25/BinaryTreeMaximumPathSum.java
@@ -6,14 +6,13 @@ public class BinaryTreeMaximumPathSum {
         if(root == null){
             return 0;
         }
-
         int fromLeft = maximumPathSum(root.left, max);
         int fromRight = maximumPathSum(root.right, max);
 
         int sumIncludingBothSides = fromLeft + fromRight + root.val;
-        max.maxSum = Math.max(max.maxSum, sumIncludingBothSides);
-
         int oneSideAndSelf = Math.max(fromLeft, fromRight) + root.val;
+        
+        max.maxSum = Math.max(max.maxSum, sumIncludingBothSides);
         max.maxSum = Math.max(max.maxSum, oneSideAndSelf);
         max.maxSum = Math.max(max.maxSum, root.val);
 
