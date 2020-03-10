b98434844a24ce8e260e59f86bfa467d1da46496
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-03-10 22:07:57 +0300

    Space optimization

--- Code Camp/PopularQuestions/LongestConsecutiveSequence.java
+++ Code Camp/PopularQuestions/LongestConsecutiveSequence.java
@@ -4,36 +4,39 @@ import java.util.HashSet;
 
 public class LongestConsecutiveSequence {
 
+
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) return 0;
+        if(nums.length == 0){
+            return 0;
+        }
         HashSet<Integer> numbers = new HashSet<>();
         HashSet<Integer> visited = new HashSet<>();
-
+        
         int maxChain = 1;
         for(int x: nums){
             numbers.add(x);
         }
-
+        
         for(int x : nums){
             if(!visited.contains(x)){
-                HashSet<Integer> members = new HashSet<>();
-                dfs(x, numbers, visited, members);
-                maxChain = Math.max(maxChain, members.size());
+                int count = dfs(x, numbers, visited);
+                maxChain = Math.max(maxChain, count);
             }
         }
         return maxChain;
     }
-
-    public void dfs(int number, HashSet<Integer> numbers, HashSet<Integer> visited, HashSet<Integer> members){
-        members.add(number);
+    
+    public int dfs(int number, HashSet<Integer> numbers, HashSet<Integer> visited){
         visited.add(number);
+        int count = 1;
+        
         if(numbers.contains(number - 1) && !visited.contains(number - 1)){
-            dfs(number - 1, numbers, visited, members);
+            count += dfs(number - 1, numbers, visited);
         }
         if(numbers.contains(number + 1) && !visited.contains(number + 1)){
-            dfs(number + 1, numbers, visited, members);
+            count += dfs(number + 1, numbers, visited);
         }
-
+        return count;
     }
 
 
