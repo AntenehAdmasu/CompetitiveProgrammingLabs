86f885cd49740c15c7e74074aa4d28fc379b3cbb
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-09-17 15:59:52 +0300

    Search insert postition using binary search

---
+++ Take2/Week 2/SearchInsertPosition.java
@@ -0,0 +1,19 @@
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
